a0baca92651084afa952c9f1321e8db45b155d18
MarcusKoen <37168149+MarcusKoen@users.noreply.github.com>
2026-01-28 11:15:50 +0200

    implementing registers

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <stdlib.h>
 //test
 using namespace std;
 
+struct CPU
+{
+
+
+    using Byte = unsigned char;//8 bits
+    using Word = unsigned short;//16 bits
+
+    Word PC; //Program Counter
+
+    Word SP;
+
+};
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
+
+
     return 0;
 }
